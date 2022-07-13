1b733370ca3b388ebf57a1daa4e6fdb62d1bc821
anoop <anoop@gmail.com>
2022-07-13 19:47:48 +0530

    final commit

---
+++ dbConn_BasicWebsite/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Student USN:</td>
+                    <td><asp:TextBox ID="usn" runat="server" OnTextChanged="usn_TextChanged" AutoPostBack="true"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td>Name</td>
+                    <td><asp:TextBox ID="name" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>City</td>
+                    <td><asp:TextBox ID="city" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Phone no</td>
+                    <td><asp:TextBox ID="Phone_no" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+                        <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click"/>
+                        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click"/>
+                        <asp:Button ID="Button4" runat="server" Text="View" OnClick="Button4_Click"/>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>Enter USN For Update</td>
+                    <td><asp:TextBox ID="usn1" runat="server" OnTextChanged="usn1_TextChanged" AutoPostBack="true"></asp:TextBox></td>
+                </tr>
+            </table>
+            <br />
+            
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ dbConn_BasicWebsite/Default.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\237598\Desktop\ASP.NET\dbConn_BasicWebsite\dbConn_BasicWebsite\App_Data\Databasedb.mdf;Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            disp_data();
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "insert into studentdb values('" + usn.Text + "','" + name.Text + "','" + city.Text + "','" + Phone_no.Text + "')";
+        cmd.ExecuteNonQuery();
+        usn.Text = "";
+        name.Text = "";
+        city.Text = "";
+        Phone_no.Text = "";
+        con.Close();
+        disp_data();
+
+    }
+
+    public void disp_data()
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "select * from studentdb";
+        cmd.ExecuteNonQuery();
+        DataTable dt = new DataTable();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+        con.Close();
+
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "delete from studentdb where usn='" + usn.Text + "' ";
+        cmd.ExecuteNonQuery();
+
+        usn.Text = "";
+
+        con.Close();
+        disp_data();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+      
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update studentdb set name= '" + name.Text + "',city='" + city.Text + "', Phone_no='" + Phone_no.Text + "' where usn='" + usn1.Text + "'";
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        usn.Text = "";
+        name.Text = "";
+        city.Text = "";
+        Phone_no.Text = "";
+        disp_data();
+       
+       
+    }
+
+    protected void usn_TextChanged(object sender, EventArgs e)
+    {
+        if(usn.Text.Length > 0)
+        {
+            usn1.Enabled = false;
+        }
+        else
+        {
+            usn1.Enabled = true;
+        }
+    }
+    protected void usn1_TextChanged(object sender, EventArgs e)
+    {
+        if (usn1.Text.Length > 0)
+        {
+            usn.Enabled = false;
+        }
+        else
+        {
+            usn.Enabled = true;
+        }
+    }
+
+
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        disp_data();
+
+    }
+}
